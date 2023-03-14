0b2684523257551c66d85156f3a237cd60a90d8c
ShazidMorshed14 <shazidmorshed14@gmail.com>
2023-03-14 08:14:23 +0600

    sumzero optimization added

--- sumzero.js
+++ sumzero.js
@@ -8,14 +8,33 @@ function sumZero(nums1) {
   for (var i = 0; i < nums.length; i++) {
     for (var j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] == 0) {
-        arr.push(i);
-        arr.push(j);
+        arr.push(nums[i]);
+        arr.push(nums[j]);
         return arr;
       }
     }
   }
 }
 
-const nums = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
-const ans = sumZero(nums); //time complexity here is o(n^2)
+function optimizeSumZero(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let arr = [];
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    if (sum === 0) {
+      arr.push(nums[left]);
+      arr.push(nums[right]);
+      return arr;
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+const nums = [-5, -4, -3, -2, 4, 9, 8, 6, 7];
+//const ans = sumZero(nums); //time complexity here is o(n^2)
+const ans = optimizeSumZero(nums); //time complexity here is o(n)
 console.log(ans);
